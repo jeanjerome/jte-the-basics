e9528dcf110e1817cfe22d713704d581c0a880d2
jeanjerome <jeanjerome.levy@gmail.com>
2022-02-24 10:06:12 +0100

    Adds dynamic volume

--- libraries/git/steps/scm_checkout.groovy
+++ libraries/git/steps/scm_checkout.groovy
@@ -1,36 +1,33 @@
 void call(){
 
-  def podTemplate = """
-  apiVersion: v1
-  kind: Pod
-  spec:
-    containers:
-    - name: alpine
-      image: alpine
-      command:
-      - sleep
-      args:
-      - infinity
-      volumeMounts:
-        - name: workspace-volume
-          mountPath: /home/jenkins/agent
-      workingDir: "/home/jenkins/agent"
-    volumes:
-        - name: "workspace-volume"
-          persistentVolumeClaim:
-            claimName: "jenkins-slave-pvc"
-            readOnly: false
-  """
-  pipeline {
-    agent {
-      kubernetes {
-        yaml podTemplate
-        workspaceVolume dynamicPVC(accessModes: 'ReadWriteOnce', requestsSize: "10Gi")
+  podTemplate(
+    containers: [
+      containerTemplate(
+        name: 'alpine', 
+        image: 'alpine:latest', 
+        command: 'sleep', 
+        args: '30d'
+      )
+    ],
+    volumes: [
+      dynamicPVC(
+        accessModes: 'ReadWriteOnce', 
+        mountPath: '/home/jenkins/agent', 
+        requestsSize: '10Gi', 
+        storageClassName: 'hostpath'
+      )
+    ]
+  ) 
+  
+  {
+    node(POD_LABEL) {
+      container('alpine') {
+        stage('checkout') {
+          deleteDir()
+          def checkout = checkout(scm)
+        }
       }
-    }
-    stage('checkout') {
-      deleteDir()
-      def checkout = checkout(scm)
-    }
+    }  
   }
+  
 }
