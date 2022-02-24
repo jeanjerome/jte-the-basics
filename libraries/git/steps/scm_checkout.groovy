731df9bac867ffe68232ff1716870019737e4762
jeanjerome <jeanjerome.levy@gmail.com>
2022-02-24 09:52:38 +0100

    Try kubernetes command for pod

--- libraries/git/steps/scm_checkout.groovy
+++ libraries/git/steps/scm_checkout.groovy
@@ -1,22 +1,36 @@
 void call(){
 
-  podTemplate(containers: [
-      containerTemplate(
-          name: 'alpine', 
-          image: 'alpine:latest', 
-          command: 'sleep', 
-          args: '30d'
-          )
-    ]) {
+  def podTemplate = """
+  apiVersion: v1
+  kind: Pod
+  spec:
+    containers:
+    - name: alpine
+      image: alpine
+      command:
+      - sleep
+      args:
+      - infinity
+      volumeMounts:
+        - name: workspace-volume
+          mountPath: /home/jenkins/agent
+      workingDir: "/home/jenkins/agent"
+    volumes:
+        - name: "workspace-volume"
+          persistentVolumeClaim:
+            claimName: "jenkins-slave-pvc"
+            readOnly: false
+  """
 
-      node(POD_LABEL) {
-        container('alpine') {
-          stage('checkout') {
-            deleteDir()
-            def checkout = checkout(scm)
-          }
-        }
-      }
-      
+  agent {
+    kubernetes {
+      yaml podTemplate
+      workspaceVolume dynamicPVC(accessModes: 'ReadWriteOnce', requestsSize: "10Gi")
     }
+  }
+  stage('checkout') {
+    deleteDir()
+    def checkout = checkout(scm)
+  }
+
 }
