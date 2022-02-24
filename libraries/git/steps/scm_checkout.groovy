ddc2656aa61b661847e74e4ba31b5279a8382b3f
jeanjerome <jeanjerome.levy@gmail.com>
2022-02-24 12:35:50 +0100

    Test

--- libraries/git/steps/scm_checkout.groovy
+++ libraries/git/steps/scm_checkout.groovy
@@ -1,11 +1,22 @@
 void call(){
 
-  podTemplate {
+  podTemplate(
+    containers: [
+      containerTemplate(
+        name: 'jnlp', 
+        image: 'jenkins/inbound-agent:latest'
+      )
+    ]
+  ) 
+  
+  {
     node(POD_LABEL) {
+      container('jnlp') {
         stage('checkout') {
           deleteDir()
           def checkout = checkout(scm)
         }
+      }
     }
   }
   
