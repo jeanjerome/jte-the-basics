c54e1799be827aba741b9ba7d66c1b3c76ab4d29
jeanjerome <jeanjerome.levy@gmail.com>
2022-02-24 01:20:56 +0100

    fixes checkout

--- libraries/git/steps/scm_checkout.groovy
+++ libraries/git/steps/scm_checkout.groovy
@@ -1,22 +1,12 @@
 void call(){
 
-  podTemplate(containers: [
-      containerTemplate(
-          name: 'alpine', 
-          image: 'alpine:latest', 
-          command: 'sleep', 
-          args: '30d'
-          )
-    ]) {
-
-      node(POD_LABEL) {
-        container('alpine') {
-          stage('checkout') {
-            deleteDir()
-            def checkout = checkout(scm)
-          }
-        }
+  podTemplate {
+    node(POD_LABEL) {
+      stage('checkout') {
+        deleteDir()
+        def checkout = checkout(scm)
       }
-      
-    }
+    } 
+  }
+
 }
